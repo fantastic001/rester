f2ec00bb3e1ee77bee86c0e847e0809cd68e9350
Stefan Nožinić <stefan@lugons.org>
2025-10-05 16:36:24 +0200

    Implement initial command structure with main execution

---
+++ rester/__main__.py
@@ -0,0 +1,10 @@
+
+from orgasm import command_executor_main
+
+class Commands(object):
+    def load(self, filename):
+        return None 
+    
+
+if __name__ == "__main__":
+    command_executor_main(Commands)
